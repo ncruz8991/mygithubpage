60e60092ccc282c56f483f1837ae4b93b9dda34a
Nick Cruz <ncruz@umich.edu>
2015-09-15 20:52:20 -0400

    Reverted previous commit

--- scrapetest/Feds to update timeline for student aid application _ The Michigan Daily_files/oqetLFFRDhq.js
+++
@@ -1,5 +0,0 @@
-/*!CK:2255505177!*//*1441682035,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["a02HG"]); }
-
-__d("ComposedEntityMutabilityToName",[],function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports={"0":"MUTABLE","1":"IMMUTABLE","2":"SEGMENTED"};},null,{});
--- scrapetest/Feds to update timeline for student aid application _ The Michigan Daily_files/sn6YnPBDNgy.js
+++
@@ -1,5 +0,0 @@
-/*!CK:3772206041!*//*1441682035,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["vj3oH"]); }
-
-__d('DraftEntityInstance',['ComposedEntityMutability','ComposedEntityType'],function a(b,c,d,e,f,g,h,i){if(c.__markCompiled)c.__markCompiled();'use strict';function j(k){this.$DraftEntityInstance1=k.type;this.$DraftEntityInstance2=k.mutability;this.$DraftEntityInstance3=k.data||{};}j.prototype.getType=function(){return i[this.$DraftEntityInstance1];};j.prototype.getMutability=function(){return h[this.$DraftEntityInstance2];};j.prototype.getData=function(){return this.$DraftEntityInstance3;};j.prototype.replaceData=function(k){this.$DraftEntityInstance3=k;};f.exports=j;},null);
